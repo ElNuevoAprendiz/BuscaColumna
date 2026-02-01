56f12652adc6d5ec3f8bdf54a88767b42eddd42e
Dany <profedanieljuan@gmail.com>
2026-02-01 00:29:24 -0300

    Primer Comit (contiene error loop infinito)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+// 1. Importamos la librería de SQLite
+import * as SQLite from 'expo-sqlite';
+
 
 export default function App() {
+
+  const db = SQLite.openDatabaseSync('miBaseDeDatos.db'); 
+
+  const [respuestaConsulta, setRespuestaConsulta] = useState<boolean>(false);
+ 
+
+  useEffect(() => {
+    setupDatabase();
+  }, []);
+
+  const setupDatabase = () => {
+    // Ejecutamos un comando SQL: CREATE TABLE IF NOT EXISTS
+    db.execSync(`
+      CREATE TABLE IF NOT EXISTS miTabla (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT NOT NULL,
+        miColumna TEXT
+      );
+    `);
+  };
+
+  const resultado = db.getAllSync<{ name: string }>(`PRAGMA table_info(miTabla)`);
+  const respuesta: boolean = resultado.some(column => column.name === 'miColumna');
+  setRespuestaConsulta(respuesta);
+
+  //console.log('¿La columna "miColumna" existe en "miTabla"?', respuestaConsulta); 
+  //console.log('Información de las columnas de "miTabla":', resultado);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>¿La columna "miColumna" existe en "miTabla"?, {respuestaConsulta ? "Sí" : "No"}</Text>
       <StatusBar style="auto" />
     </View>
   );
